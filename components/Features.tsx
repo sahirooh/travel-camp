78388eccac3ccd73c2c84ccfd95ac874022555e6
sahirooh <sahirooh@gmail.com>
2023-12-05 19:24:09 +0530

    added features

--- components/Features.tsx
+++ components/Features.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
+    <section className='border-2 border-black flex-col flexCenter
+    overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24'>
+      <div className="feature-phone" />
+      
+    </section>
+  );
 }
 
 export default Features
