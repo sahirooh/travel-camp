dd903cf021ad2daccfe8332b78c8f23872f074b5
sahirooh <sahirooh@gmail.com>
2023-12-05 23:59:35 +0530

    added getapp

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
-  )
+    <section className="max-container border-2 border-black">
+      <div>
+        <Image
+          className="w-full get-app 2xl:rounded-5xl"
+          src="/phones.png"
+          alt="boat"
+          width={1000}
+          height={880}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default GetApp
